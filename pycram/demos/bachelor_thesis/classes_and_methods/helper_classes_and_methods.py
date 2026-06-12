b44ce81606b2d3242782d08501f66fa4cb88a86c
Hanna Becker <beckerh@uni-bremen.de>
2026-06-13 00:12:28 +0200

    fixed array issue in calculating robot world comparison

--- pycram/demos/bachelor_thesis/classes_and_methods/helper_classes_and_methods.py
+++ pycram/demos/bachelor_thesis/classes_and_methods/helper_classes_and_methods.py
@@ -162,9 +162,9 @@ def compare_robot_world_with_real(dispatcher: EventDispatcher, world: World, con
     real_world_dispatcher.known_furniture = dispatcher.known_furniture
 
     # dispatcher gets all semantically annotated objects in the world -> same case as if robot has found all objects
-    real_world_dispatcher.perceived_objects = dispatcher.perceived_objects
-    real_world_dispatcher.misplaced_objects = dispatcher.misplaced_objects
-    real_world_dispatcher.reachable_objects = dispatcher.reachable_objects
+    real_world_dispatcher.perceived_objects = dispatcher.perceived_objects.copy()
+    real_world_dispatcher.misplaced_objects = dispatcher.misplaced_objects.copy()
+    real_world_dispatcher.reachable_objects = dispatcher.reachable_objects.copy()
     real_world_dispatcher.trigger_event(world.bodies, world, context=context)
 
     result = _print_task_comparison_robot_real(dispatcher, real_world_dispatcher)
